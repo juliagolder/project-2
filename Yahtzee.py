ae7d38ec5c167a9da7e28322df480af7bf7ab7a1
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-25 08:23:11 -0400

    Updated from Brython Server: 5/25/2018 8:23:12 AM

--- Yahtzee.py
+++ Yahtzee.py
@@ -12,22 +12,22 @@ def printRoll(dice):
     print(dice)
     ask1 = input('roll die 1 again?')
     if ask1 == 'yes' or ask1 == 'y':
-        cha1 = input('what would you like to change it to?')
+        cha1 = randint(1,6)
         L[0]=cha1
         print(L)
     ask2 = input('roll die 2 again?')
     if ask2 == 'yes' or ask2 == 'y':
-        cha2 = input('what would you like to change it to?')
+        cha2 = randint(1,6)
         L[1]=cha2
         print(L)
     ask3 = input('roll die 3 again?')
     if ask3 == 'yes' or ask3 == 'y':
-        cha3 = input('what would you like to change it to?')
+        cha3 = randint(1,6)
         L[2]=cha3
         print(L)
     ask4 = input('roll die 4 again?')
     if ask4 == 'yes' or ask4 == 'y':
-        cha4 = input('what would you like to change it to?')
+        cha4 = randint(1,6)
         L[3]=cha4
         print(L)
     ask5 = input('roll die 5 again?')
