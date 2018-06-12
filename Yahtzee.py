aedd7dbdcf38e49e1b3942f7171b1ad705c6c18c
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-06-12 08:17:37 -0400

    Updated from Brython Server: 6/12/2018 8:17:37 AM

--- Yahtzee.py
+++ Yahtzee.py
@@ -69,7 +69,7 @@ def enterScore(num, dice, scoreL): #tallies up score of the roll
             score = 50
         else:
             score = 0
-    scoreL[num]
+    scoreL[num-1]
 
 def is3ofakind(L): #function for determining rolls with three of a kind
     for i in range(1,7):
